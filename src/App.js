3a52f94ad228ba468f0f71872571f6134c3c471c
AlfaIF <jamg@discente.ifpe.edu.br>
2022-11-22 00:02:53 -0300

    Adicionando estilos e variáveis

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [senha, criarSenha] = useState('')
+  const [tamanhoSenha, criarTamanhoSenha] = useState(20)
+  const [incluirUppercase, criarComUppercase] = useState(false)
+  const [incluirLowercase, criarComLowercase] = useState(false)
+  const [incluirNumeros, criarComNumeros] = useState(false)
+  const [incluirSimbolos, criarComSimbolos] = useState(false)
+
   return <div className="App">
     <div className="container">
       <div className="gerador">
         <h2 className="cabeçalho">
           Gerador de Senhas
         </h2>
+
         <div className="gerador-de-senhas">
-          <h3>Senha</h3>
+          <h3>senha</h3>
           <button className="btn_copia">
             <i className='far fa-clipboard'></i>
           </button>
         </div>
+
         <div className="form-group">
           <label htmlFor="tamanho-senha">Tamanho da senha</label>
-          <input type="number" id="tamanho-senha" name="tamanho-senha" max={32} min={10} />
+          <input 
+            defaultValue={criarTamanhoSenha}
+            onChange={(e) => criarTamanhoSenha(e.target.value)}
+            type="number"
+            id="tamanho-senha"
+            name="tamanho-senha"
+            max={32} min={10} />
         </div>
+
         <div className="form-group">
           <label htmlFor="letra-maiuscula">Letras maiúsculas</label>
-          <input type="checkbox" id="letra-maiuscula" name="letra-maiuscula"/>
+          <input 
+            checked={incluirUppercase}
+            onChange={(e) => criarComUppercase(e.target.checked)}
+            type="checkbox"
+            id="letra-maiuscula"
+            name="letra-maiuscula"/>
         </div>
+
         <div className="form-group">
           <label htmlFor="letra-minúscula">Letras minúsculas</label>
-          <input type="checkbox" id="letra-minúscula" name="letra-minúscula"/>
+          <input 
+            checked={incluirLowercase}
+            onChange={(e) => criarComLowercase(e.target.checked)}
+            type="checkbox"
+            id="letra-minúscula"
+            name="letra-minúscula"/>
         </div>
+
         <div className="form-group">
           <label htmlFor="números">Números</label>
-          <input type="checkbox" id="números" name="números"/>
+          <input 
+            checked={incluirNumeros}
+            onChange={(e) => criarComNumeros(e.target.checked)}
+            type="checkbox" 
+            id="números" 
+            name="números"/>
         </div>
+
         <div className="form-group">
           <label htmlFor="símbolos">Símbolos</label>
-          <input type="checkbox" id="símbolos" name="símbolos"/>
+          <input
+            checked={incluirSimbolos}
+            onChange={(e) => criarComSimbolos(e.target.checked)} 
+            type="checkbox" 
+            id="símbolos" 
+            name="símbolos"/>
         </div>
+
         <button className="btn_gerador">Gerar senha</button>
       </div>
+
       <div />
     </div>
   </div>
